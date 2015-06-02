5c6c4cbc9c0e0ea0a780ce3ccf9123041c0ff96c
Jonathan D. Simms <jsimms@twitter.com>
2015-06-02 20:53:30 +0000

    keep built readline around

--- bootstrap_python.sh
+++ bootstrap_python.sh
@@ -1,12 +1,18 @@
-INSTALL_ROOT=$HOME/Python
+INSTALL_ROOT="$HOME/Python"
 CPY=$INSTALL_ROOT/CPython
 PYPY=$INSTALL_ROOT/PyPy
+
+READLINE_URL="ftp://ftp.cwru.edu/pub/bash/readline-6.2.tar.gz"
+READLINE_FILENAME="$(basename $READLINE_URL)"
+READLINE_VERNAME="${READLINE_FILENAME%%.tar.gz}"
+
+READLINE_PREFIX="$INSTALL_ROOT/$READLINE_VERNAME"
 
 SANDBOX=$(mktemp -d /tmp/python.XXXXXX)
 
 CURL='wget --no-check-certificate'
 
-mkdir -p $INSTALL_ROOT
+mkdir -p $INSTALL_ROOT "$READLINE_PREFIX"
 
 PYTHON_2_6=2.6.9
 PYTHON_2_7=2.7.9
@@ -17,10 +23,10 @@ SETUPTOOLS=12.0.5
 PIP=6.0.8
 
 pushd $SANDBOX
-  wget ftp://ftp.cwru.edu/pub/bash/readline-6.2.tar.gz
-  tar xzf readline-6.2.tar.gz
+  wget "$READLINE_URL"
+  tar xzf "$READLINE_FILENAME"
   pushd readline-6.2
-    ./configure --disable-shared --enable-static --prefix=$SANDBOX/readline
+    ./configure --disable-shared --enable-static --prefix="$READLINE_PREFIX"
     make -j3 && make install
   popd
   rm -rf readline-6.2.tar.gz readline-6.2
@@ -30,12 +36,12 @@ pushd $SANDBOX
     $CURL http://python.org/ftp/python/$version/Python-$version.tgz
     tar xzf Python-$version.tgz
     pushd Python-$version
-      LDFLAGS=-L$SANDBOX/readline/lib CFLAGS=-I$SANDBOX/readline/include \
+      LDFLAGS=-L"$READLINE_PREFIX/lib" CFLAGS=-I"$READLINE_PREFIX/include" \
         ./configure --prefix=$INSTALL_ROOT/CPython-$version && make -j5 && make install
     popd
     rm -f Python-$version.tgz
   done
-  
+
   # install pypy
   for pypy_version in $PY_PY-osx64; do
     pushd $INSTALL_ROOT
@@ -48,7 +54,7 @@ pushd $SANDBOX
 
   $CURL https://pypi.python.org/packages/source/s/setuptools/setuptools-$SETUPTOOLS.tar.gz
   $CURL http://pypi.python.org/packages/source/p/pip/pip-$PIP.tar.gz
-  
+
   for interpreter in $CPY-$PYTHON_2_6/bin/python2.6 \
                      $CPY-$PYTHON_2_7/bin/python2.7 \
                      $CPY-$PYTHON_3_3/bin/python3.3 \
@@ -63,7 +69,7 @@ pushd $SANDBOX
       rm -rf $base
     done
   done
-  
+
   rm -f setuptools-$SETUPTOOLS.tar.gz pip-$PIP.tar.gz
 popd
 
